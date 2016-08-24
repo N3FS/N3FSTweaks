80d55402838d778cd555a2da3ebfb556947ce9c2
md678685 <mathew678@hotmail.co.uk>
2016-08-24 17:49:14 +0100

    Initial code commit

---
+++ src/main/java/co/n3fs/spigottweaks/N3FSTweaks.java
@@ -0,0 +1,15 @@
+package co.n3fs.spigottweaks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class N3FSTweaks extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+}
