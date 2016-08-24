cb226fdf29678d01505850291d352c00d85a83c4
md678685 <mathew678@hotmail.co.uk>
2016-08-24 18:28:26 +0100

    Register listener at startup

--- src/main/java/co/n3fs/spigottweaks/N3FSTweaks.java
+++ src/main/java/co/n3fs/spigottweaks/N3FSTweaks.java
@@ -1,15 +1,18 @@
 package co.n3fs.spigottweaks;
 
+import co.n3fs.spigottweaks.listener.PlayerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class N3FSTweaks extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        this.getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
     }
 
     @Override
     public void onDisable() {
+        
     }
 
 }
